9f4b024378b61e7135b069ba609fe8b63ff57217
EsraOzdogan <50195220+EsraOzdogan@users.noreply.github.com>
2020-03-15 01:10:42 +0300

    .

---
+++ WcfService/IService1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService
+{
+  
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string Hosgeldiniz(string ad);
+        [OperationContract]
+        string GeçersizKullanıcı();
+        [OperationContract]
+        string FavorilereEklendi();
+        [OperationContract]
+        double ekleme(double n1, double n2);
+        [OperationContract]
+        double silme(double n1, double n2);
+
+    }
+
+
+}
